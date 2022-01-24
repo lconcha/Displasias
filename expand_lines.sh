12fc95ace4a6dec6f1d02872bfb26acfa3e674b6
Luis Concha <lconcha@gmail.com>
2022-01-24 14:03:14 -0600

    first commit

---
+++ expand_lines.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source `which my_do_cmd`
+
+
+multiline_volume=$1
+rat=$2
+outfolder=$3
+
+
+my_do_cmd mrcalc $multiline_volume 1 -eq ${outfolder}/${rat}_l_outline.nii.gz
+my_do_cmd mrcalc $multiline_volume 2 -eq ${outfolder}/${rat}_r_outline.nii.gz
+my_do_cmd mrcalc $multiline_volume 3 -eq ${outfolder}/${rat}_l_inline.nii.gz
+my_do_cmd mrcalc $multiline_volume 4 -eq ${outfolder}/${rat}_r_inline.nii.gz
