a7d1e7de34b33b9b3db9750f4395834469f82f92
Luis Concha <lconcha@gmail.com>
2022-01-26 17:51:34 -0600

    added help

--- expand_lines.sh
+++ expand_lines.sh
@@ -7,7 +7,7 @@ rat=$2
 outfolder=$3
 
 
-my_do_cmd mrcalc $multiline_volume 1 -eq ${outfolder}/${rat}_l_outline.nii.gz
-my_do_cmd mrcalc $multiline_volume 2 -eq ${outfolder}/${rat}_r_outline.nii.gz
-my_do_cmd mrcalc $multiline_volume 3 -eq ${outfolder}/${rat}_l_inline.nii.gz
-my_do_cmd mrcalc $multiline_volume 4 -eq ${outfolder}/${rat}_r_inline.nii.gz
+my_do_cmd mrcalc -quiet $multiline_volume 1 -eq ${outfolder}/${rat}_l_outline.nii.gz
+my_do_cmd mrcalc -quiet $multiline_volume 2 -eq ${outfolder}/${rat}_r_outline.nii.gz
+my_do_cmd mrcalc -quiet $multiline_volume 3 -eq ${outfolder}/${rat}_l_inline.nii.gz
+my_do_cmd mrcalc -quiet $multiline_volume 4 -eq ${outfolder}/${rat}_r_inline.nii.gz
