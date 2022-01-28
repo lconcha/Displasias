2fb802892be67c15972e8f63d9206255f7e418b2
Luis Concha <lconcha@gmail.com>
2022-01-28 13:17:01 -0600

    fixed misalignment of lines wrt t2 by fslcpgeom

--- nii2streams_prepareOrientation.sh
+++ nii2streams_prepareOrientation.sh
@@ -30,11 +30,25 @@ t2=$2
 rat=$3
 outfolder=$4
 
+if [ ! -d $outfolder ]
+then
+  echolor red "ERROR Outfolder does not exist: $outfolder"
+  exit 2
+fi
+
 
 tmpDir=`mktemp -d`
 
 
-# modify strides of the lines
+# first make sure we have the same transformation matrices between t2 and lines
+my_do_cmd mrconvert -quiet $t2 ${tmpDir}/t2.nii.gz 
+my_do_cmd mrconvert -quiet $lines ${tmpDir}/lines.nii.gz
+my_do_cmd fslcpgeom ${tmpDir}/lines.nii.gz ${tmpDir}/t2.nii.gz
+lines=${tmpDir}/lines.nii.gz
+t2=${tmpDir}/t2.nii.gz
+
+
+# modify strides
 plines=${outfolder}/lines.nii.gz
 size1=$(mrinfo -size $lines | awk '{print $1}')
 size2=$(mrinfo -size $lines | awk '{print $2}')
@@ -43,20 +57,27 @@ echolor yellow "sizes: $size1 $size2 $size3"
 if [ $size2 -lt $size3 ]
 then
   echolor cyan "Warning: size2 is larger than size3. Will permute dimensions of lines"
-  my_do_cmd mrconvert -axes 0,2,1 \
+  my_do_cmd mrconvert -quiet -axes 0,2,1 \
             -strides 1,2,3 \
             $lines \
             ${tmpDir}/plines_wtransf.nii.gz
-  my_do_cmd mrtransform -identity ${tmpDir}/plines_wtransf.nii.gz ${tmpDir}/plines.nii.gz
+  my_do_cmd mrtransform -quiet -identity ${tmpDir}/plines_wtransf.nii.gz ${tmpDir}/plines.nii.gz
+  echolor cyan "Warning: Also permuting dimensions of t2"
+  my_do_cmd mrconvert -quiet -axes 0,2,1 \
+            -strides 1,2,3 \
+            $t2 \
+            ${tmpDir}/t2_wtransf.nii.gz
+  my_do_cmd mrtransform -quiet -identity ${tmpDir}/t2_wtransf.nii.gz ${outfolder}/t2.nii.gz
         
 else
   cp $lines ${tmpDir}/plines.nii.gz
+  cp $t2 ${outfolder}/t2.nii.gz
 fi
 
 
 # make sure the T2 has the same geometry
-my_do_cmd mrconvert $t2 ${outfolder}/${rat}_t2.nii.gz
-my_do_cmd fslcpgeom ${tmpDir}/plines.nii.gz ${outfolder}/${rat}_t2.nii.gz
+#my_do_cmd mrconvert -quiet $t2 ${outfolder}/${rat}_t2.nii.gz
+#my_do_cmd fslcpgeom ${tmpDir}/plines.nii.gz ${outfolder}/${rat}_t2.nii.gz
 
 
 
@@ -88,6 +109,10 @@ do
 done
 
 
+mrcat -quiet -axis 3 \
+  $outfolder/??/${rat}_?_inline.nii.gz \
+  $outfolder/??/${rat}_?_outline.nii.gz - | \
+  mrmath -quiet -axis 3 - max ${outfolder}/lines.nii.nii.gz
 
 #echolor cyan "Try running, for example:
 # nii2streams.sh ${outfolder}/${rat}_l_inline.nii.gz ${outfolder}/${rat}_l_outline.nii.gz ${outfolder}/${rat}_l_inline.nii.gz ${outfolder} l $rat"
