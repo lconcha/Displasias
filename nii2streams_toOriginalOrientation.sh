55bc1b6b5fd5b3df13378ffd6e195c6c959762e5
Luis Concha <lconcha@gmail.com>
2022-03-10 13:10:31 -0600

    first commit

--- nii2streams_toOriginalOrientation.sh
+++ nii2streams_toOriginalOrientation.sh
@@ -12,8 +12,6 @@ tmpDir=$(mktemp -d)
 refdims=$(mrinfo -size $imref | tr ' ' ',')
 refstrides=$(mrinfo -strides $imref | tr ' ' ',')
 
-echolor yellow $refdims
-echolor yellow $refstrides
 
 my_do_cmd mrconvert -axes 0,2,1 -strides $refstrides $imtofix ${tmpDir}/permuted.nii
 my_do_cmd mrgrid -size $refdims ${tmpDir}/permuted.nii regrid ${tmpDir}/regrid.nii
