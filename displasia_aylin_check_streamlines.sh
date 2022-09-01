33e099e7601ee7bf6a9ed8d605687e9c14c79635
Luis Concha <lconcha@gmail.com>
2022-09-01 08:56:59 -0500

    first commit

--- displasia_aylin_check_streamlines.sh
+++ displasia_aylin_check_streamlines.sh
@@ -24,18 +24,36 @@ then
 fi
 
 
+if [ -z "$tck" -o -z "$img" ]
+then
+  echolor red "[ERROR] Files are not OK."
+  echolor red "        Check $rat"
+  exit 2
+fi
+
+
+
+
 nslice=$(echo `basename $tck` | cut -d _ -f 3)
-nstreamlines=$(tckinfo -count $tck | grep ' count:' | cut -d : -f 2 | sed 's/\s//g')
+nstreamlines=$(tckinfo -quiet -count $tck | grep ' count:' | cut -d : -f 2 | sed 's/\s//g')
 
 echolor yellow " Streamlines are in slice $nslice"
 echolor yellow " There are $nstreamlines streamlines"
 
 seq 1 ${nstreamlines} > /tmp/indices.txt
 
+capture_folder=/tmp/captures
+mkdir $capture_folder
 my_do_cmd mrview $img \
   -tractography.load $tck  \
   -tractography.tsf_load /tmp/indices.txt \
   -mode 1 \
   -voxel 0,0,${nslice}  \
-  -plane 2
+  -plane 2 \
+  -capture.folder $capture_folder \
+  -capture.prefix ${rat} \
+  -capture.grab \
+  -exit
+
+  echo "capture folder is $capture_folder"
 
