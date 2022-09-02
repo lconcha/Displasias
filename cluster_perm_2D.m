be8bc97ad9793dc4794fe076fb559adb0ae4dbf5
Luis Concha <lconcha@gmail.com>
2022-09-02 11:28:38 -0500

    now returns cluster labels

--- cluster_perm_2D.m
+++ cluster_perm_2D.m
@@ -1,6 +1,7 @@
 function pcluster = cluster_perm_2D(groupA,groupB,...
                     ndiffperms,nclusperms,...
                     clusterformingpthreshold,...
+                    clusterpthreshold,...
                     conn,...
                     doPlot)
 
@@ -13,6 +14,15 @@ function cpvals = findclusterpvals(cluster_sizes,clusternulldist)
         n = numel(find(clusternulldist>thisclustersize));
         cpvals(c) = n./numel(clusternulldist);
     end
+end
+
+function labels_pval = paintclusterpval(clabels,cpvals)
+    labels_pval = ones(size(clabels));
+    for c = 1 : length(cpvals)
+       thiscpval = cpvals(c);
+       labels_pval(find(clabels==c)) = thiscpval;
+    end
+
 end
 
 
@@ -33,7 +43,7 @@ randclustersizes = [];
 
 fprintf(1,'Building empirical distribution of cluster sizes that occur by chance alone with %d permutations\n',nclusperms)
 for perm = 1 : nclusperms
-    fprintf(1,'%d ',round(100*(perm/nclusperms)))
+    if mod(perm,10) == 0; fprintf(1,'%d ',round(100*(perm/nclusperms)));end
     permutation = randperm(size(groupAB,3));
     idx1 = permutation(1:ngA);
     idx2 = permutation(ngA+1:end);
@@ -56,8 +66,33 @@ pcluster.cluster_pvals.AltB    = findclusterpvals(pcluster.clustersizes.AltB,ran
 pcluster.cluster_pvals.AdiffB  = findclusterpvals(pcluster.clustersizes.AdiffB,randclustersizes);
 pcluster.cluster_pvals.Student = findclusterpvals(pcluster.clustersizes.Student,randclustersizes);
 
+pcluster.cluster_pvals_2D.AgtB    = paintclusterpval(pcluster.clusterlabels.AgtB,   pcluster.cluster_pvals.AgtB);
+pcluster.cluster_pvals_2D.AltB    = paintclusterpval(pcluster.clusterlabels.AltB,   pcluster.cluster_pvals.AltB);
+pcluster.cluster_pvals_2D.AdiffB  = paintclusterpval(pcluster.clusterlabels.AdiffB, pcluster.cluster_pvals.AdiffB);
+pcluster.cluster_pvals_2D.Student = paintclusterpval(pcluster.clusterlabels.Student,pcluster.cluster_pvals.Student);
+
 if doPlot
-  fprintf(1, 'plots not coded yet\n')
+    cmap_div  = uint8(cbrewer('div','PuOr',128, 'spline') .* 255);
+    cmap_warm = uint8(cbrewer('seq','YlOrBr',128,'spline') .* 255); 
+    cmap_cool = uint8(cbrewer('seq','PuBuGn',128,'spline') .* 255); cmap_cool = flip(cmap_cool,1);
+    cmap_pval = hot(128); cmap_pval = flip(cmap_pval,1);
+    cmap_flag = prism(50); cmap_flag(1,:) = [1 1 1];
+
+    figure;
+    cmin = min([groupA(:);groupB(:)]);
+    cmax = max([groupA(:);groupB(:)]);
+    subplot(3,3,1);imagesc(mean(groupA,3)');set(gca,'Clim',[cmin cmax]);colorbar;title('A'); set(gca,'colormap',cmap_cool);
+    subplot(3,3,2);imagesc(mean(groupB,3)');set(gca,'Clim',[cmin cmax]);colorbar;title('B'); set(gca,'colormap',cmap_cool);
+    subplot(3,3,3);imagesc(pcluster.diff')';colorbar;title('A-B'); set(gca,'colormap',cmap_div);
+    lims = get(gca,'Clim');
+        newlims = [-abs(max(lims)) max(lims)];
+        set(gca,'Clim',newlims);
+    subplot(3,3,4);imagesc(pcluster.pAgtB');  set(gca,'Clim',[0 0.05]);colorbar;title(['pAgtB ('   num2str(ndiffperms) ' perms)']); set(gca,'colormap',cmap_pval);
+    subplot(3,3,5);imagesc(pcluster.pAltB');  set(gca,'Clim',[0 0.05]);colorbar;title(['pAltB ('   num2str(ndiffperms) ' perms)']); set(gca,'colormap',cmap_pval);
+    subplot(3,3,6);imagesc(pcluster.pAdiffB');set(gca,'Clim',[0 0.05]);colorbar;title(['pAdiffB (' num2str(ndiffperms) ' perms)']); set(gca,'colormap',cmap_pval);
+    subplot(3,3,7);imagesc(pcluster.cluster_pvals_2D.AgtB');set(gca,'Clim',[0 0.05]);colorbar;title(['pcluster AgtB ('   num2str(nclusperms) ' perms)']); set(gca,'colormap',cmap_pval);
+    subplot(3,3,8);imagesc(pcluster.cluster_pvals_2D.AltB');set(gca,'Clim',[0 0.05]);colorbar;title(['pcluster AltB ('   num2str(nclusperms) ' perms)']); set(gca,'colormap',cmap_pval);
+    subplot(3,3,9);imagesc(pcluster.Student');      set(gca,'Clim',[0 0.05]);colorbar;title('Student t'); set(gca,'colormap',cmap_pval);
 end
 
 
