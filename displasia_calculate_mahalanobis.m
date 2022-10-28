e98f03f11025fed0f609e82c5e3a04afffb73149
Luis Concha <lconcha@gmail.com>
2022-10-27 19:18:17 -0500

    first

--- displasia_calculate_mahalanobis.m
+++ displasia_calculate_mahalanobis.m
@@ -1,5 +1,5 @@
 [status,hname]= unix('hostname');
-hname = deblank(hname)
+hname = deblank(hname);
 
 switch hname
     case 'mansfield'
@@ -26,36 +26,21 @@ end
 
 load(f_results);
 
-s = 25;
-d = 5;
+takeTheseMetrics = [1 2 10 11 12 13 14];
 idx_ctrl = RESULTS.data.idx_ctrl;
 idx_bcnu = RESULTS.data.idx_bcnu;
-
-takeTheseMetrics = zeros(21,1);
-takeTheseMetrics([2 10]) = 1;
-takeTheseMetrics = logical(takeTheseMetrics);
-nMetricsTaken    = sum(takeTheseMetrics(:));
-
-values_ctrl = squeeze(RESULTS.data.DATA(s,d,idx_ctrl,takeTheseMetrics));
-values_bcnu = squeeze(RESULTS.data.DATA(s,d,idx_bcnu,takeTheseMetrics));
-values_all  = squeeze(RESULTS.data.DATA(s,d,:,takeTheseMetrics));
-
-
-d_ctrl = values_ctrl ./ mean(values_all);
-d_bcnu = values_bcnu ./ mean(values_all);
-m_b = mahal(d_bcnu,d_ctrl);
-m_c = mahal(d_ctrl,d_ctrl);
-
-scatter(d_ctrl(:,1), d_ctrl(:,2), 50, m_c, 'Marker','x'); hold on
-scatter(d_bcnu(:,1), d_bcnu(:,2), 50, m_b , 'filled')
-
 
 
 SE = strel('diamond',1); % create a 4-conn neighborhood
 
 nStreamlines = size(RESULTS.data.DATA,1);
 nDepths      = size(RESULTS.data.DATA,2);
+nRats        = size(RESULTS.data.DATA,3);
+nMetrics     = length(takeTheseMetrics);
+
+MAHAL   = zeros(nStreamlines,nDepths,nRats);
 for s = 1 : nStreamlines
+    fprintf(1,'%d of %d\n',s,nStreamlines);
     for d = 1 : nDepths
        im = zeros(nStreamlines,nDepths);
        im(s,d) = 1;
@@ -63,12 +48,30 @@ for s = 1 : nStreamlines
        [i,j] = ind2sub(size(imd),find(imd));
        
        npix = sum(imd(:));
-       values_ctrl = zeros()
 
-       values_ctrl = squeeze(RESULTS.data.DATA(i,j,idx_ctrl,takeTheseMetrics)); % esto está mal, quiero la coincidencia i,j
-       values_bcnu = squeeze(RESULTS.data.DATA(i,j,idx_bcnu,takeTheseMetrics));
-       values_all  = squeeze(RESULTS.data.DATA(i,j,:,takeTheseMetrics));
+       nbhood = zeros(npix,nRats,nMetrics);
+       for p = 1 : npix
+          nbhood(p,:,:) = squeeze(RESULTS.data.DATA(i(p),j(p),:,takeTheseMetrics));
+       end
+       nbhood = squeeze(mean(nbhood,1)); % average values for each rat.
+       
+       values_ctrl = nbhood(idx_ctrl,:);% values_ctrl = reshape(values_ctrl, npix*sum(idx_ctrl),nMetrics);
+       values_bcnu = nbhood(idx_bcnu,:);% values_bcnu = reshape(values_bcnu, npix*sum(idx_bcnu),nMetrics);
+       values_all  = nbhood; %reshape(nbhood,  npix* nRats,       nMetrics);
+
+       d_ctrl = values_ctrl ./ mean(values_all);
+       d_bcnu = values_bcnu ./ mean(values_all);
+       d_all  = values_all ./ mean(values_all);
+       m_b = mahal(d_bcnu,d_ctrl);
+       m_c = mahal(d_ctrl,d_ctrl);
+       m_a = mahal(d_all, d_ctrl);
+       %scatter(d_ctrl(:,1), d_ctrl(:,2), 50, m_c, 'Marker','x'); hold on
+       %scatter(d_bcnu(:,1), d_bcnu(:,2), 50, m_b , 'filled')
+
+       MAHAL(s,d,:) = m_a;
     end
 end
 
 
+
+displasia_show_streamlines_with_values(f_tck, mean(MAHAL(:,:,idx_bcnu),3)  ,[], 'Mahal')
