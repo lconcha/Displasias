9300bfd8cada9776f591b61d77c609791cf5bbbd
Luis Concha <lconcha@gmail.com>
2023-02-03 19:26:04 -0600

    simplify sampling of several input files using cell array

--- displasia_tckfixelsample.m
+++ displasia_tckfixelsample.m
@@ -1,15 +1,35 @@
-function VALUES = displasia_tckfixelsample(f_tck, f_PDD, f_nComp, f_values_in, ...
-                                           f_tsf_par_out, f_tsf_perp_out, f_tsf_par_index_out)
-% VALUES = displasia_tcksample(tck,f_values_in,f_txt_out)
-
+function VALUES = displasia_tckfixelsample(f_tck, f_PDD, f_nComp, ff_values_in, f_prefix)
+% VALUES = displasia_tckfixelsample(f_tck, f_PDD, f_nComp, ff_values_in, f_prefix)
+%
+% f_tck         : Filename for the streamlines tck
+% f_PDD         : Filename for the Principal Diffusion Directions file (MRDS, 4D).
+% f_nComp       : Filename for the number of components (MRDS, 3D).
+% ff_values_in  : Cell array of filenames of MRDS metrics to sample. 
+%                 Each file should be MRDS, 4D.
+% f_prefix      : Prefix for the output file names.
+%
+% Consider:
 % addpath('/home/lconcha/software/mrtrix_matlab/matlab');
 % addpath(genpath('/home/lconcha/software/dicm2nii-master'))
 % addpath /home/lconcha/software/Displasias/
-
-%%%%%% bash
-%mrgrid -template roi_26_15_47.nii dwi_MRDS_Diff_BIC_NUM_COMP.nii.gz regrid regrid.nii -strides roi_26_15_47.nii -force
-%%%%%%%
-%f_MRDS_ncomp = 'dwi/roi_26_15_47.nii';
+%
+% __________________________________________________________________________________
+% EXAMPLE:
+% f_tck         = 'dwi/15/tck/dwi_l_out_resampled_native.tck';
+% f_PDD         = 'dwi/dwi_MRDS_Diff_BIC_PDDs_CARTESIAN.nii.gz';
+% f_MRDS_ncomp  = 'dwi/dwi_MRDS_Diff_BIC_NUM_COMP.nii.gz';
+% f_MRDS_FA     = 'dwi/dwi_MRDS_Diff_BIC_FA.nii.gz';
+% f_MRDS_MD     = 'dwi/dwi_MRDS_Diff_BIC_MD.nii.gz';
+% ff_values     = {f_MRDS_FA, f_MRDS_MD};
+% f_prefix      = '/tmp/prefix';
+% 
+% VALUES = displasia_tckfixelsample(f_tck, f_PDD, f_MRDS_ncomp, ff_values, f_prefix);
+% __________________________________________________________________________________
+%
+% LU15 (0N(H4
+% INB-UNAM
+% Feb 2023
+% lconcha@unam.mx
 
 
 fprintf('Loading %s\n',f_tck);
@@ -45,9 +65,9 @@ if size(PDD,2) > size(PDD,3)
   fprintf(1,'Woah, it seems like slices in the PDD file are in the third dimension. For displasia project they should be on the second dimension.\n');
   fprintf(1,'  ... will convert the file to have correct strides for you outside of matlab.\n')
   tmpPDD = '/tmp/PDD.nii.gz';
-  systemcommand = ['mrconvert -strides 1,2,3,4 ' f_PDD ' ' tmpPDD];
+  systemcommand = ['mrconvert -strides 1,2,3,4 -quiet ' f_PDD ' ' tmpPDD];
   fprintf(1,'  executing: %s\n',systemcommand);
-  [status,result] = system(systemcommand)
+  [status,result] = system(systemcommand);
   fprintf('Loading %s\n',tmpPDD);
   PDD = niftiread(tmpPDD);
   infoPDD = niftiinfo(tmpPDD);
@@ -57,7 +77,7 @@ if size(nComp,2) > size(nComp,3)
   fprintf(1,'Woah, it seems like slices in the nComp file are in the third dimension. For displasia project they should be on the second dimension.\n');
   fprintf(1,'  ... will convert the file to have correct strides for you outside of matlab.\n')
   tmpnComp = '/tmp/nComp.nii.gz';
-  systemcommand = ['mrconvert -strides 1,2,3 ' f_nComp ' ' tmpnComp];
+  systemcommand = ['mrconvert -strides 1,2,3 -quiet ' f_nComp ' ' tmpnComp];
   fprintf(1,'  executing: %s\n',systemcommand);
   [status,result] = system(systemcommand);
   fprintf('Loading %s\n',tmpnComp);
@@ -152,84 +172,93 @@ fprintf (1,'\nFinished identifying par/perp\n',s);
 
 
 %% Do the sampling
-
-fprintf('Loading %s\n',f_values_in);
-V    = niftiread(f_values_in);
-info = niftiinfo(f_values_in);
-if ndims(V) ~= 4
-    fprintf(1,'ERROR. %s does not have 4 dimensions. This script can only handle 4D. Bye.\n',f_values_in);
-    VALUES = [];
-    return
-end
-if size(V,2) > size(V,3)
-  fprintf(1,'Woah, it seems like slices in the volume to sample from are in the third dimension. For displasia project they should be on the second dimension.\n');
-  fprintf(1,'  ... will convert the file to have correct strides for you outside of matlab.\n')
-  tmpvaluesfile = '/tmp/tmpvaluesfile.nii.gz';
-  systemcommand = ['mrconvert -strides 1,2,3,4 ' f_values_in ' ' tmpvaluesfile];
-  fprintf(1,'  executing: %s\n',systemcommand);
-  [status,result] = system(systemcommand);
-  fprintf('Loading %s\n',tmpvaluesfile);
-  V = niftiread(tmpvaluesfile);
-  info = niftiinfo(tmpvaluesfile);
-  [status,result] = system(['rm -f ' tmpvaluesfile]);
-end
-
-for s = 1 : length(tck.data)
-   fprintf (1,'%d ',s);
-   this_streamline = tck.data{s};
-   this_data_par        = zeros(size(this_streamline,1),1);
-   this_data_perp       = zeros(size(this_streamline,1),1);
-   for p = 1 : size(this_streamline,1);
-       xyz = this_streamline(p,:);
-       
-       vox_indices = [xyz 1]  * inv(info.Transform.T);
-       vox_indices = vox_indices(1:3);
-       mindices    = vox_indices + 1;
-       matlab_indices = uint8(vox_indices + 1);
-
-     
-       %thisnComp = interp3(nComp,mindices(2), mindices(1), mindices(3), 'nearest');
-       thisnComp  = tsf_ncomp.data{s}(p);
-       indexpar   = tsf_index_par.data{s}(p);
-       indexperp  = tsf_index_perp.data{s}(p);
-
-       
-       vals(1) = interp3(V(:,:,:,1),mindices(2), mindices(1), mindices(3));
-       vals(2) = interp3(V(:,:,:,2),mindices(2), mindices(1), mindices(3));
-       vals(3) = interp3(V(:,:,:,3),mindices(2), mindices(1), mindices(3));
-
-       if thisnComp < 3
-        vals(thisnComp+1:end) = -1; % remove values if nComp does not support them.
+for i = 1 : length(ff_values_in)
+    f_values_in = ff_values_in{i};
+    fprintf('Loading %s\n',f_values_in);
+    V    = niftiread(f_values_in);
+    info      = niftiinfo(f_values_in);
+    [fold,fname,ext] = fileparts(info.Filename);
+    varName = strrep(fname,'.nii','');
+    if ndims(V) ~= 4
+        fprintf(1,'ERROR. %s does not have 4 dimensions. This script can only handle 4D. Bye.\n',f_values_in);
+        VALUES = [];
+        return
+    end
+    if size(V,2) > size(V,3)
+      fprintf(1,'Woah, it seems like slices in the volume to sample from are in the third dimension. For displasia project they should be on the second dimension.\n');
+      fprintf(1,'  ... will convert the file to have correct strides for you outside of matlab.\n')
+      tmpvaluesfile = '/tmp/tmpvaluesfile.nii.gz';
+      systemcommand = ['mrconvert -strides 1,2,3,4 -quiet ' f_values_in ' ' tmpvaluesfile];
+      fprintf(1,'  executing: %s\n',systemcommand);
+      [status,result] = system(systemcommand);
+      fprintf('Loading %s\n',tmpvaluesfile);
+      V = niftiread(tmpvaluesfile);
+      info = niftiinfo(tmpvaluesfile);
+      [status,result] = system(['rm -f ' tmpvaluesfile]);
+    end
+    
+    for s = 1 : length(tck.data)
+       %fprintf (1,'%d ',s);
+       this_streamline = tck.data{s};
+       this_data_par        = zeros(size(this_streamline,1),1);
+       this_data_perp       = zeros(size(this_streamline,1),1);
+       for p = 1 : size(this_streamline,1);
+           xyz = this_streamline(p,:);
+           
+           vox_indices = [xyz 1]  * inv(info.Transform.T);
+           vox_indices = vox_indices(1:3);
+           mindices    = vox_indices + 1;
+           matlab_indices = uint8(vox_indices + 1);
+    
+         
+           %thisnComp = interp3(nComp,mindices(2), mindices(1), mindices(3), 'nearest');
+           thisnComp  = tsf_ncomp.data{s}(p);
+           indexpar   = tsf_index_par.data{s}(p);
+           indexperp  = tsf_index_perp.data{s}(p);
+    
+           
+           vals(1) = interp3(V(:,:,:,1),mindices(2), mindices(1), mindices(3));
+           vals(2) = interp3(V(:,:,:,2),mindices(2), mindices(1), mindices(3));
+           vals(3) = interp3(V(:,:,:,3),mindices(2), mindices(1), mindices(3));
+    
+           if thisnComp < 3
+            vals(thisnComp+1:end) = -1; % remove values if nComp does not support them.
+           end
+    
+           if max(vals) < 0
+            fprintf(1,'WTF? All values are invalid!')
+            fprintf(1,'Streamline %d, point %d\n',s,p);
+            vals
+           end
+    
+           val_par  = vals(indexpar);
+           val_perp = vals(indexperp);
+    
+           this_data_par(p,1)  = val_par;
+           this_data_perp(p,1) = val_perp;       
        end
-
-       if max(vals) < 0
-        fprintf(1,'WTF? All values are invalid!')
-        fprintf(1,'Streamline %d, point %d\n',s,p);
-        vals
+       tsf_par.data{s}  = this_data_par;
+       tsf_perp.data{s} = this_data_perp;
+       try
+        tsf_index_par.data{s} = this_index_par;
+       catch
+        fprintf(1,'Hey!')
        end
+    end
+    fprintf (1,'\nFinished sampling %s\n',fname);
+    
+    
+    f_tsf_par_out  = [f_prefix '_' varName '_par.tsf'];
+    f_tsf_perp_out = [f_prefix '_' varName '_perp.tsf'];
+    fprintf(1,'  [INFO] Writing tsf_par: %s\n',f_tsf_par_out);
+    write_mrtrix_tsf(tsf_par,f_tsf_par_out)
+    fprintf(1,'  [INFO] Writing tsf_perp: %s\n',f_tsf_perp_out);
+    write_mrtrix_tsf(tsf_perp,f_tsf_perp_out)
 
-       val_par  = vals(indexpar);
-       val_perp = vals(indexperp);
-
-       this_data_par(p,1)  = val_par;
-       this_data_perp(p,1) = val_perp;       
-   end
-   tsf_par.data{s}  = this_data_par;
-   tsf_perp.data{s} = this_data_perp;
-   try
-    tsf_index_par.data{s} = this_index_par;
-   catch
-    fprintf(1,'Hey!')
-   end
+    VALUES.par.(varName)  = tsf_par.data;
+    VALUES.perp.(varName) = tsf_perp.data;
 end
-fprintf (1,'\nFinished sampling\n',s);
 
-
-fprintf(1,'Writing tsf_par: %s\n',f_tsf_par_out);
-write_mrtrix_tsf(tsf_par,f_tsf_par_out)
-fprintf(1,'Writing tsf_perp: %s\n',f_tsf_perp_out);
-write_mrtrix_tsf(tsf_perp,f_tsf_perp_out)
-fprintf(1,'Writing tsf_index_par: %s\n',f_tsf_par_index_out);
+f_tsf_par_index_out = [f_prefix '_par_index.tsf'];
+fprintf(1,'  [INFO] Writing tsf_index_par: %s\n',f_tsf_par_index_out);
 write_mrtrix_tsf(tsf_index_par,f_tsf_par_index_out)
-fclose(fid);
-VALUES = tsf_par.data;
