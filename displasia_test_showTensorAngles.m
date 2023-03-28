d5fbd7fbb5d6eb8c02013677aba762b16e2400bd
Luis Concha <lconcha@gmail.com>
2023-03-27 19:57:17 -0600

    testing with different files

--- displasia_test_showTensorAngles.m
+++ displasia_test_showTensorAngles.m
@@ -27,10 +27,19 @@ switch hname
         addpath('/home/lconcha/software/mrtrix_matlab/matlab');
         addpath(genpath('/home/lconcha/software/dicm2nii-master'))
         addpath /home/lconcha/software/Displasias/
-        f_tck         = '/datos/syphon/displasia/testMRDS/dwi/one.tck';
-        f_PDD         = '/datos/syphon/displasia/testMRDS/dwi/regrid_dwi_MRDS_Diff_BIC_PDDs_CARTESIAN.nii.gz';
-        f_nComp       = '/datos/syphon/displasia/testMRDS/dwi/regrid_dwi_MRDS_Diff_BIC_NUM_COMP.nii.gz';
-        ff_values_in  = {'/datos/syphon/displasia/testMRDS/dwi/regrid_dwi_MRDS_Diff_BIC_FA.nii.gz'};
+        addpath(genpath('/home/lconcha/software/matGeom'))
+
+        %f_tck         = '/datos/syphon/displasia/testMRDS/dwi/one.tck';
+        
+        f_tck = '/datos/syphon/displasia/testMRDS_david/37A_l_14_out_resampled_10.tck';
+        %f_tck = '/datos/syphon/displasia/testMRDS_david/voxspace.tck';
+        mrds_basename = '/datos/syphon/displasia/testMRDS_david/37A_mrds_MRDS_Diff_BIC';
+
+        f_PDD         = [mrds_basename, '_PDDs_CARTESIAN.nii.gz'];
+        f_nComp       = [mrds_basename, '_NUM_COMP.nii.gz'];
+        %f_nComp       = '/datos/syphon/displasia/testMRDS_david/7_3.nii';
+        %ff_values_in  = {[mrds_basename, '_FA.nii.gz']};
+        ff_values_in = {['/datos/syphon/displasia/testMRDS_david/fake_MRDS_FA.nii.gz']}
         f_prefix      =  '/tmp/tests_MRDS';
 
 
