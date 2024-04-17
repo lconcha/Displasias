716c11013182424322cc4d8cd1b0dbfe0432d8b0
Luis Concha <lconcha@gmail.com>
2024-04-16 19:54:21 -0600

    now making cluster plots

--- displasia_tempscript.m
+++ displasia_tempscript.m
@@ -1,10 +1,36 @@
 
+addpath(genpath('/home/lconcha/software/Displasias'));
 
-tiledlayout("flow")
 
-for r = 1 : nRats
-  thisrat = DATA.Values(:,:,r,1);
-  imagesc(thisrat)'
-  title(DATA.IDs(r));
-  nexttile
-end
+%%%%%%% OPTIONS %%%%%%%%%%%%%%%
+f_csv = 'example_files/uFAp_long.csv';
+clusterformingpthreshold = 0.01;
+clusterpthreshold        = 0.02;
+ndiffperms               = 1000;
+nclusperms               = 1000;
+conn                     = 4;
+doPlot                   = true;
+rng(1);                  % seed. Note: added this line only AFTER the first 20 metrics (and mahal) were analyzed, so it is pointless. My bad.
+hemi                     = 1; % 1=left, 2=right
+thistitle                = 'test with uFA';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+DATA = displasia_oli_table2matrix(f_csv);
+
+groupA.data = DATA.Values(:,:,DATA.index.ctrl,hemi);
+groupA.name = 'ctrl';
+groupB.data = DATA.Values(:,:,DATA.index.bcnu,hemi);
+groupB.name = 'bcnu';
+
+pcluster = displasia_oli_cluster_perm_2D(groupA,groupB,...
+                ndiffperms,nclusperms,...
+                clusterformingpthreshold,...
+                clusterpthreshold,...
+                conn,...
+                thistitle,...
+                doPlot);
+
+subplot(331); set(gca,'CLim',[0 1])
+subplot(332); set(gca,'CLim',[0 1])
+subplot(333); set(gca,'CLim',[-0.2 0.2])
