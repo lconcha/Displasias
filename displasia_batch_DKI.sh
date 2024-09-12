e2b13e283d588663a724bee65cb3e922cf78151c
Luis Concha <lconcha@gmail.com>
2024-09-12 13:35:15 -0600

    DKI through dipy

---
+++ displasia_batch_DKI.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+imagesdir=/misc/carr2/paulinav/Displasia_project/displasia
+logsdir=/misc/carr2/paulinav/Displasia_project/logs
+fails=${logsdir}/failed_DKI.txt
+if [ -f $fails ]; then rm $fails;fi
+
+
+
+
+for grp in CTRL BCNU
+do
+  for dr in ${imagesdir}/${grp}/*
+  do
+    rat=$(basename $dr)
+    for dd in ${imagesdir}/${grp}/${rat}/*
+    do
+      day=$(basename $dd)
+      dwi=${imagesdir}/${grp}/${rat}/${day}/${rat}_${day}_${grp}_hibval_deb.nii.gz
+      bval=${dwi%.nii.gz}.bval
+      bvec=${dwi%.nii.gz}.bvec
+      mask=${imagesdir}/${grp}/${rat}/${day}/derivatives/dwi/${rat}_${day}_${grp}_DTInolin_FA.nii.gz
+      outdir=${imagesdir}/${grp}/${rat}/${day}/derivatives/dwi/dki
+      if [ ! -f $dwi ]; then echo $dwi >> $fails;fi
+      fsl_sub -N dki_${rat}${day} -l $logsdir -s smp,6 \
+        displasia_run_DKI.sh $dwi $bval $bvec $mask $outdir
+    done
+  done
+done
+
---
+++ displasia_run_DKI.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+dwi=$1
+bval=$2
+bvec=$3
+mask=$4
+outdir=$5
+
+echo "start"
+
+fcheck=${outdir}/mk.nii.gz
+echolor cyan "Looking for $fcheck"
+if [ ! -f $fcheck ]
+then
+    echolor cyan "  DKI output not found. Will run dipy_fit_dki"
+    dipy_fit_dki \
+    --b0_threshold 40 \
+    --out_dir $outdir \
+    $dwi $bval $bvec $mask
+else
+    echolor green "Kurtosis ran. File exists: $fcheck"
+fi
+
+
+# quick Quality Check, there should be few voxels with negative kurtosis
+mk=${outdir}/mk.nii.gz
+ak=${outdir}/ak.nii.gz
+rk=${outdir}/rk.nii.gz
+if [ -f $mk ]
+then
+    if [ -f ${outdir}/bad_mk.nii.gz ]
+    then
+        echolor green "QC ran, File exists: ${outdir}/bad_mk.nii.gz "
+    else
+        echolor cyan "Running quick QC"
+        for f in $mk $ak $rk
+        do
+        mrcalc $f 0 -lt ${outdir}/bad_$(basename $f)
+        done
+    fi
+fi
